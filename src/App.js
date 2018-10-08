3b98f8f286e2c0dc6b2716aff744cc201e210d3d
Maria Caruso <mariakcaruso@gmail.com>
2018-10-08 02:38:26 +0000

    add purecss and sample hamburger menu

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App" id="layout">
+              <header className="App-header pure-u-1">West Philly Coffee Shops</header>
+              <a href="#menu" id="menuLink" className="menu-link">
+		<span></span>
+	      </a>
+              <div id="menu" className="">
+                <div className="pure-menu">
+                  <a className="pure-menu-heading" href="#">Company</a>
+                  <ul className="pure-menu-list">
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>                  
+                    <li className="pure-menu-item menu-item-divided pure-menu-selected">
+                      <a href="#" className="pure-menu-link">Services</a>
+                    </li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        );
+    }
 }
 
 export default App;
