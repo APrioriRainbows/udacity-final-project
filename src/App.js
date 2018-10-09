485e6e6446f05333e6560bc0ac2119aecc1560b6
Maria Caruso <mariakcaruso@gmail.com>
2018-10-09 01:30:19 +0000

    add in location data file and remove hardcoded data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './components/map.js'
 import Script from 'react-load-script'
-
 class App extends Component {
+    state = {
+	AllLocations: [
+	    //The Green Line Cafe
+	    {location: {}, googleID:'ChIJBQodP_DGxokR7BU-LHhMz7w'},
+	    //Jezebel\'s Studio
+	    {location: {}, googleID:'ChIJLxn5DfDGxokRrhtvTyksLPo'},
+	    //Reanimator Coffee 
+	    {location: {}, googleID:'ChIJFTrn7e7GxokR-hMwClmetUU'},
+	    //Satellite Cafe 
+	    {location: {}, googleID:'ChIJT8YkwurGxokRrG38CigmHRU'},
+	    //Saxby\'s'
+	    {location: {}, googleID:'ChIJKVKKvvfGxokRVsWjTa4tMqk'},
+	    //Earthen Cup Cafe'
+	    {location: {}, googleID:'ChIJ7ZuY5vHGxokRNt9zUNldZb4'},	    
+	],
+	FilteredLocation: []
+    }
+    filterLocation = () => {
+    }
     render() {
         return (
             <div className="App" id="layout">
               <header className="App-header pure-u-1">West Philly Coffee Shops</header>
-		<Map />
+		<Map locations={this.state.AllLocations}/>
               <a href="#menu" id="menuLink" className="menu-link">
 		<span></span>
 	      </a>
--- src/components/map.js
+++ src/components/map.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import MAPS_API_KEY from '../keys.js';
 import Script from 'react-load-script';
+
 export default class Map extends Component {
+    constructor(props){
+	super(props);
+    }
     state = {
-        LocationList: [
-            { name: 'asldkfj',
-              position: {lat: 37.7749300, lng: -122.4194200}, // I think this neeeds a special syntax
-              title: 'Hello World!'
-            }
-        ]
+        LocationList: []
     }
     handleScriptLoad() {
         this.map = new window.google.maps.Map(document.getElementById('map'), {
             zoom: 12,
             center: {lat: 37.7749300, lng: -122.4194200}
         });
+
+        const locations = this.props.locations;
+        const locationIDs =locations.map(location => location.googleID);
         this.state.LocationList.map(location => {
             const marker = new window.google.maps.Marker(
                 { position: location.position,
                   map: this.map,
                   title: location.title }
-            )
+            );
             return marker
-        })
+        });
     }
     render(){
-        const key = `https://maps.googleapis.com/maps/api/js?key=${MAPS_API_KEY}`
+        const key = `https://maps.googleapis.com/maps/api/js?key=${MAPS_API_KEY}&libraries=places`
             return(
                 <div className="pure-u-1">
                   <Script
