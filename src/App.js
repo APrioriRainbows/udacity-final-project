f5fc5f020fb300a9b533ce9dc9a8b98d81505791
Maria Caruso <mariakcaruso@gmail.com>
2018-10-13 17:26:16 +0000

    add in filter menu and function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     }
     state = {
 	AllLocations: [],
-	FilteredLocation: []
+	FilteredLocations: []
     }
 
     filterLocation = () => {
@@ -19,26 +19,42 @@ class App extends Component {
 
     componentDidMount(){
     	this.setState({AllLocations: ALL_LOCATIONS})
+        this.setState({FilteredLocations: ALL_LOCATIONS})
+
+    }
+    filterLocations(e){
+        e.preventDefault();
+        console.warn(this)
+        const AllLocations = this.state.AllLocations;
+        let val = document.querySelector('input').value.trim().toLowerCase();
+        console.log(val)
+        let FilteredLocations = AllLocations.filter(location => location.name.trim().toLowerCase().includes(val));
+        this.setState({FilteredLocations: FilteredLocations});
     }
 
     render() {
+        const FilteredLocations = this.state.FilteredLocations
         return (
             <div className="App" id="layout">
               <header className="App-header pure-u-1">West Philly Coffee Shops</header>
-		<Map locations={this.state.AllLocations}/>
+		<Map locations={FilteredLocations}/>
               <a href="#menu" id="menuLink" className="menu-link">
 		<span></span>
 	      </a>
               <div id="menu" className="">
                 <div className="pure-menu">
                   <a className="pure-menu-heading" href="#">Company</a>
+		  <form className="pure-form" onSubmit={e => this.filterLocations(e)}>
+		    <fieldset>
+		      <input type="text" className="pure-input-rounded"/>
+		      <button type="submit" className="pure-button">Filter</button>
+                    </fieldset>
+                  </form>
                   <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>                  
-                    <li className="pure-menu-item menu-item-divided pure-menu-selected">
-                      <a href="#" className="pure-menu-link">Services</a>
-                    </li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
+		    {FilteredLocations.map(location =>
+                        <li className="pure-menu-item" key={location.name}><a href="#" className="pure-menu-link">{location.name}</a></li>
+                    )
+                    }
                   </ul>
                 </div>
               </div>
