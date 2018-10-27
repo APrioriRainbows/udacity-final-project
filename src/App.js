24f67a51f0c8385f2720ecc1facd474eff083d78
Maria Caruso <mariakcaruso@gmail.com>
2018-10-27 20:17:56 +0000

    add error handling for foursquare

--- src/App.js
+++ src/App.js
@@ -9,29 +9,40 @@ class App extends Component {
     state = {
 	AllLocations: [],
 	FilteredLocations: [],
-        ActiveLocation:''
+        ActiveLocation:'',
+        PageErrors: []
     }
     componentDidMount(){
     	this.setState({AllLocations: ALL_LOCATIONS});
-        this.setState({FilteredLocations: ALL_LOCATIONS});
+        this.setState({FilteredLocations: ALL_LOCATIONS})
+        let errorLog = [];
+	const fetches = []
         for (let fsq of fourSquareLocationData) {
             const venueId = fsq.venueId;
             //takes in client id and client secret for the foursquare api. PLEASE INSERT YOUR OWN IF REVIEWING.
             const qs = `&client_id=${keys.FS_CLIENT_ID}&client_secret=${keys.FS_CLIENT_SECRET}`;
             const v = "&v=20181020";
             const url = `https://api.foursquare.com/v2/venues/${venueId}/photos?limit=1`+qs+v;
-            fetch(url).then((response) => response.json()).then(json => {
-                for (let location of this.state.AllLocations) {                    
-                    if (location.name === fsq.venueName) {
-                        let photo = json.response.photos.items[0] || undefined;
-                        if(photo){
-                            location.photoURL = `${photo.prefix}200x200${photo.suffix}`;
+	    //push all the fetch requests into an array that will eventually be used in a promise.all call
+            fetches.push(
+		//if the request succeeds set the photo info
+		fetch(url).then((response) => response.json()).then(json => {
+                    let photo = json.response.photos.items[0];
+                    if (!photo) { return }
+                    for (let location of this.state.AllLocations) {
+			if (location.name === fsq.venueName) {
+			    location.photoURL = `${photo.prefix}200x200${photo.suffix}`;
                         }
                     }
-                }
-            })
-                .catch(err => console.error('Caught error: ', err));
+		})
+		//if the request fails, catch the error and push it to an array. Will not throw an error.
+		//since the request is returning photos, then there is a default photo set in map.js in case of failure
+		    .catch(err => errorLog.push(err))
+	    )
         }
+	//Then do promise.all to fetch all the requests. This takes in the array of requests.
+	//Next run a function to set the state with the erros or lack of errors. This triggers the alert to show the list of errors.
+	Promise.all(fetches).then(() => this.setState({PageErrors: errorLog}))
      }
     //this function takes in query string and filters down all available locations to match query string.
     //sets a state variable with that array of locations to be used elsewhere
@@ -52,9 +63,15 @@ class App extends Component {
         const layout = document.getElementById('layout');
         layout.classList.toggle('active');
     }
+    displayAlert(errors) {
+	const error_text = errors.join("\n")
+        alert(`Darn. There were some issues loading the foursquare API. Those were: \n${error_text}\n Please note that some pictures may not load due to these errors.`);
+    }
     render() {
         let FilteredLocations = this.state.FilteredLocations;
         const ActiveLocation = this.state.ActiveLocation;
+        let PageErrors = this.state.PageErrors;
+        PageErrors.length && this.displayAlert(PageErrors);
         return (
             <div className="App" id="layout">
               <header className="App-header pure-u-1">West Philly Coffee Shops</header>
